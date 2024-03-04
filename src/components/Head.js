8c19b113e7b430bad58ab9e9a8bc59f1a156009e
Mursheed <mursheed.khan@qsstechnosoft.com>
2024-03-05 00:26:42 +0530

    Search, Caching , debouncing

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,14 +1,55 @@
-import React from "react"
-import { useDispatch } from "react-redux"
+import React, { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
 import { toggleMenu } from "../utils/appSlice";
+import {cacheResults} from "../utils/searchSlice";
+import {YOUTUBE_SEARCH_API} from "../utils/constants";
+
 const Head = () => {
 
     const dispatch = useDispatch();
-    
+    const [searchQuery, setSearchQuery] = useState("");
+    const [suggestions, setSuggestions] = useState([]);
+    const [showSuggestions, setShowSuggestions] = useState(false);
 
+    const searchCache = useSelector(store=> store.search)
+
+    useEffect(()=>{
+
+       // Apply logic for debouncing as well as caching 
+       const timer = setTimeout(() => {
+            if(searchCache[searchQuery]){
+                setSuggestions(searchCache[searchQuery])
+            }else{
+                getSearchSuggestions();
+            }
+            
+        }, 200);
+       
+       return ()=>{
+        clearTimeout(timer);
+       }
+    },[searchQuery])
+
+    const getSearchSuggestions = async()=>{
+        console.log("API CALL: " + searchQuery);
+        const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+
+        const json =await data.json();
+
+        dispatch(
+            cacheResults({
+                [searchQuery]:json[1]
+            })
+        );
+        setSuggestions(json[1]);
+        
+    }
+
+    
     const toggleMenuHandler = ()=>{
         dispatch(toggleMenu())
     }
+
   return (
     <div className="grid grid-flow-col p-5 m-2 shadow-lg">
         <div className="flex col-span-1">
@@ -27,11 +68,30 @@ const Head = () => {
             </a>
             
         </div>
-        <div className="flex col-span-10 px-10">
-            <input className="w-1/2 border border-gray-400 p-2 rounded-l-full" type="text" />
-            <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
-              🔍  
-            </button>
+        <div className="col-span-10 px-10">
+            <div>
+                <input 
+                    type="text"
+                    name="search"
+                    value={searchQuery}
+                    onChange={(e)=>setSearchQuery(e.target.value)}
+                    onFocus={()=>setShowSuggestions(true)}
+                    onBlur={()=>setShowSuggestions(false)}
+                    className="w-1/2 border border-gray-400 p-2 rounded-l-full" 
+                />
+                <button 
+                className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
+                🔍  
+                </button>
+            </div>
+            {showSuggestions && <div className="absolute bg-white mx-2 py-2 px-5 w-1/3">
+                <ul>
+                    {suggestions.map(s=><li key={s} className="py-2 shadow-sm hover:bg-gray-100">🔍 {s}</li>)}
+                                        
+                </ul>
+
+            </div>}
+            
         </div>
         <div className="flex col-span-1">
             <img 
