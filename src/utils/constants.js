d5cbc7ca4d7fd18dcd868badb148f6792b416de5
Mursheed <mursheed.khan@qsstechnosoft.com>
2024-03-06 22:12:48 +0530

    New Key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,8 @@
-const GOOGLE_API_KEY = "AIzaSyAw6kmckBgwCPk6C_skGFNTlAlfyn867vQ";
+const GOOGLE_API_KEY = "AIzaSyDmJw-ilfqiCus67mKiJFATTzIYys8Ur_Y";
+
+//AIzaSyAw6kmckBgwCPk6C_skGFNTlAlfyn867vQ
+
+// https://console.cloud.google.com/apis/credentials/key/666e35d7-00c3-441c-84bd-780f756bb44f?project=emaily-387104
 
 export const LIVE_CHAT_COUNT = 25;
 
